3e4a4418e18af137f5ae9585a1b1cc9016042a46
Daniel Knüppe <daniel_k@posteo.net>
2018-12-15 01:41:24 +0100

    still not working...

--- devember2k18/Src/main.c
+++ devember2k18/Src/main.c
@@ -3,6 +3,7 @@
 
 void SystemClock_Config(void);
 void DAC_Init();
+void TIM6_Init();
 
 int main(void)
 {
@@ -14,6 +15,7 @@ int main(void)
         sine[i] = 2028 + 2000 * (sin(((2*pi)/res)*i));
     }
 
+    TIM6_Init();
     DAC_Init();
     DAC1->CR |= 0x1U;
 
@@ -27,9 +29,17 @@ int main(void)
 
 void DAC_Init()
 {
-    DAC1->CR = 0x0U;
-    DAC1->
     RCC->APB1LENR |= RCC_APB1LENR_DAC12EN;
+    DAC1->CR = 0x0U;
     DAC1->MCR = 0x2U;
+    DAC1->CR |= 0x2U; //Trig enable
+    DAC1->CR |= (0x101U << 2); //TIM6 Trig
     DAC1->CCR = 0x12000eU;
+}
+
+void TIM6_Init()
+{
+    RCC->APB1LENR |= RCC_APB1LENR_TIM6EN;
+    TIM6->CR1 = 0x101U;
+    TIM6->PSC = 0xFFFFU;
 }
