4cc9b0c84db13ceb936bb05323cb30c10e6aa652
Daniel Knüppe <daniel_k@posteo.net>
2018-12-21 00:18:11 +0100

    Still not right makefile...

--- devember2k18/Src/main.cpp
+++ devember2k18/Src/main.cpp
@@ -1,1 +1,28 @@
-main.c
+#include <math.h>
+#include "stm32h743xx.h"
+#include "stm32h7-dac.hpp"
+
+void SystemClock_Config(void){;}
+
+int main(void)
+{
+    const double pi = acos(-1);
+    const int res = 36;
+    uint32_t i = 0;
+    uint16_t sine[res];
+    for(int i = 0; i < res; i++){
+        sine[i] = (int)(2028 + 2000 * (sin(((2*pi)/res)*i)));
+    }
+
+    stm32::DAC<stm32::DacChannel::chan1> dac;
+    //dac.enable();
+    //dac.enableChannel1();    
+
+    while (true)
+    {
+        //volatile size_t test = sizeof(stm32::dacType);
+        i++;
+        i = i % res;
+        //dac.test(sine[i]);
+    }
+}
